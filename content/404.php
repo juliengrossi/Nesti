af4bed093f9d861a3e363f2b0e626f0a45d75899
Julien Grossi <52457544+juliengrossi@users.noreply.github.com>
2022-01-26 14:52:54 +0100

    3rd strike
    added 404

---
+++ content/404.php
@@ -0,0 +1,14 @@
+<section class="page-section">
+    <div class="container">
+      <div class="product-item">
+        <div class="product-item-title d-flex">
+          <div class="bg-faded p-5 d-flex ml-auto rounded">
+            <h2 class="section-heading mb-0">
+              <span class="section-heading-upper">OUPS</span>
+              <span class="section-heading-lower">Erreur 404 :)</span>
+            </h2>
+          </div>
+        </div>        
+      </div>
+    </div>
+  </section>
