a11d6a1e2ba66f83a8981e0000889b6dc60aee0e
Julien Grossi <52457544+juliengrossi@users.noreply.github.com>
2022-01-26 16:43:50 +0100

    5th whatever
    added mediaqueries for text size

--- common/head.php
+++ common/head.php
@@ -12,3 +12,4 @@
     <!-- Core theme CSS (includes Bootstrap)-->    
     <link href="css/styles.css" rel="stylesheet" />
     <link href="css/business-casual.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet" type="text/css"/>
